4153edfe58e1e6b1c701eac6ec96d90a962bdbc4
josekadeveloper <josekadeveloper@gmail.com>
2023-09-11 12:57:04 +0200

    add functions and constants

--- tic-tac-toe/src/utils/functions.ts
+++ tic-tac-toe/src/utils/functions.ts
@@ -1,21 +1,17 @@
-// Helper function to calculate the winner
-export const calculateWinner = (squares: string[]): string | null => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-  
-    for (const [a, b, c] of lines) {
+import { WINNER_COMBOS } from "./constans";
+
+// Helper function to get the winner
+export const getWinner = (squares: string[]): string | null => {  
+    for (const [a, b, c] of WINNER_COMBOS) {
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
     }
   
     return null;
-  };
+};
+
+// Helper function to check for a draw
+export const checkDraw = (squares: string[]): boolean => {
+  return squares.filter(cell => cell === null).length === 0;
+}
