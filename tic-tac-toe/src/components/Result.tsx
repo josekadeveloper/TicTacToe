111e8dcfd5b07373788daf2483aee686fb0c592c
josekadeveloper <josekadeveloper@gmail.com>
2023-09-11 12:56:09 +0200

    feat(Result): add Result component

---
+++ tic-tac-toe/src/components/Result.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface ResultProps {
+    winner: string | boolean | null;
+    resetGame: () => void;
+}
+
+const Result: React.FC<ResultProps> = ({ winner, resetGame }) => {
+    if (winner === null) return null;
+
+    const winnerText = winner === false ? 'Draw' : 'Winner: ';
+
+    return (
+        <section className='winner'>
+            <div className='text'>
+                <h2>{winnerText}</h2>
+
+                <header className='win'>
+                    {winner && <div className='cell'>{winner}</div>}
+                </header>
+
+                <footer className='new-game'>
+                    <button onClick={resetGame}>New Game</button>
+                </footer>
+            </div>
+        </section>
+    );
+};
+
+export default Result;
