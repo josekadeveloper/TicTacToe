4e1ef6b0c1dc4f1e84d4c488b4e6b89a713479df
josekadeveloper <josekadeveloper@gmail.com>
2023-09-11 12:54:03 +0200

    feat(Board): add Board component

---
+++ tic-tac-toe/src/components/Board.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Square from './Square';
+
+interface BoardProps {
+    handleClick: (idx: number) => void;
+    board: string[];
+}
+
+const Board: React.FC<BoardProps> = ({ handleClick, board }) => {
+    return (
+        <section className='board'>
+            {board.map((_, index) => (
+                <Square key={index} index={index} handleClick={handleClick} board={board} />
+            ))}
+        </section>
+    );
+};
+
+export default Board;
